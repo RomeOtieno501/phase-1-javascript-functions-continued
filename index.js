e9bb052ee456cdd2d3a4fe2aa64f734471cf418a
Rome Ojuro Otieno <otienorome2@gmail.com>
2024-09-26 10:37:20 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!"
+
 // code your solution here
